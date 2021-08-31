97ab5f94ab2149ef990adf0cdc2f004b1d3e98d9
unknown <rapozao2007@gmail.com>
2021-08-31 13:27:21 -0300

    mais uma atuali

---
+++ pages/land_page/js/style.js
@@ -0,0 +1,71 @@
+document.querySelector('.section-8 .perguntas .title1').addEventListener('click', () => {
+    if (document.querySelector('.p1').style.display == 'block') {
+        document.querySelector('.p1').style.display = 'none'
+        document.querySelector('.icon-1').classList.remove('fa-angle-down')
+        document.querySelector('.icon-1').classList.add('fa-angle-right')
+    } else {
+        document.querySelector('.p1').style.display = 'block'
+        document.querySelector('.icon-1').classList.remove('fa-angle-right')
+        document.querySelector('.icon-1').classList.add('fa-angle-down')
+    }
+})
+
+document.querySelector('.section-8 .perguntas .title2').addEventListener('click', () => {
+    if (document.querySelector('.p2').style.display == 'block') {
+        document.querySelector('.p2').style.display = 'none'
+        document.querySelector('.icon-2').classList.remove('fa-angle-down')
+        document.querySelector('.icon-2').classList.add('fa-angle-right')
+    } else {
+        document.querySelector('.p2').style.display = 'block'
+        document.querySelector('.icon-2').classList.remove('fa-angle-right')
+        document.querySelector('.icon-2').classList.add('fa-angle-down')
+    }
+})
+
+document.querySelector('.section-8 .perguntas .title3').addEventListener('click', () => {
+    if (document.querySelector('.p3').style.display == 'block') {
+        document.querySelector('.p3').style.display = 'none'
+        document.querySelector('.icon-3').classList.remove('fa-angle-down')
+        document.querySelector('.icon-3').classList.add('fa-angle-right')
+    } else {
+        document.querySelector('.p3').style.display = 'block'
+        document.querySelector('.icon-3').classList.remove('fa-angle-right')
+        document.querySelector('.icon-3').classList.add('fa-angle-down')
+    }
+})
+
+document.querySelector('.section-8 .perguntas .title4').addEventListener('click', () => {
+    if (document.querySelector('.p4').style.display == 'block') {
+        document.querySelector('.p4').style.display = 'none'
+        document.querySelector('.icon-4').classList.remove('fa-angle-down')
+        document.querySelector('.icon-4').classList.add('fa-angle-right')
+    } else {
+        document.querySelector('.p4').style.display = 'block'
+        document.querySelector('.icon-4').classList.remove('fa-angle-right')
+        document.querySelector('.icon-4').classList.add('fa-angle-down')
+    }
+})
+
+document.querySelector('.section-8 .perguntas .title5').addEventListener('click', () => {
+    if (document.querySelector('.p5').style.display == 'block') {
+        document.querySelector('.p5').style.display = 'none'
+        document.querySelector('.icon-5').classList.remove('fa-angle-down')
+        document.querySelector('.icon-5').classList.add('fa-angle-right')
+    } else {
+        document.querySelector('.p5').style.display = 'block'
+        document.querySelector('.icon-5').classList.remove('fa-angle-right')
+        document.querySelector('.icon-5').classList.add('fa-angle-down')
+    }
+})
+
+document.querySelector('.section-8 .perguntas .title6').addEventListener('click', () => {
+    if (document.querySelector('.p6').style.display == 'block') {
+        document.querySelector('.p6').style.display = 'none'
+        document.querySelector('.icon-6').classList.remove('fa-angle-down')
+        document.querySelector('.icon-6').classList.add('fa-angle-right')
+    } else {
+        document.querySelector('.p6').style.display = 'block'
+        document.querySelector('.icon-6').classList.remove('fa-angle-right')
+        document.querySelector('.icon-6').classList.add('fa-angle-down')
+    }
+})
